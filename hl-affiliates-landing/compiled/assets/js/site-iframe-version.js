148c68e51e7d9a63f2db3b3e0c268db9788d441d
Andrew Pham <apham@point.agency>
2016-01-18 11:56:13 +1100

    Getting the loading/re-loading/resetting of iframes during transitions right.

--- hl-affiliates-landing/compiled/assets/js/site-iframe-version.js
+++ hl-affiliates-landing/compiled/assets/js/site-iframe-version.js
@@ -4,10 +4,20 @@ $(document).ready(function(){
 
 	ImageContainerFadeIn();
 
+	var initiated = false;
+
 	var $slideControls = $('#slide-selectors');
 
 	window.highlightControls = function () {
 		$slideControls.addClass('highlight');
+	}
+
+	window.alternateControlsColor = function () {
+		$slideControls.addClass('alternate');
+	}
+
+	window.normalControlsColor = function() {
+		$slideControls.removeClass('alternate');
 	}
 
 
@@ -15,30 +25,117 @@ $(document).ready(function(){
 		console.log('nextSlide called');
 	}
 
+
+	var checkNewFrameReadyInterval,
+		checkPreviousFrameInterval;
+
 	window.enterSlide = function(target) {
-		$frame = target,
+
+		$frame = $(target),
 		frameWindow = window.frames[target.replace('#','')];
 
+		clearInterval(checkNewFrameReadyInterval);
 
-		$('.slide-iframe').removeClass('active');
+		clearInterval(checkPreviousFrameInterval);
 
-		$frame.addClass('active');
-		frameWindow.enter();
+		previousFrameWindow = window.frames[$('.slide-iframe.active').attr('name')];
 
+		checkPreviousFrameInterval = setInterval(function(){
+			if(previousFrameWindow.isReady) {
+				window.clearInterval(checkPreviousFrameInterval);
 
-		
+				if(previousFrameWindow.finishedAnimating) {
+
+					if(previousFrameWindow.exit) {
+
+						console.log('--TRANSITIONING: previous Frame has exit');
+
+						previousFrameWindow.exit(function(){
+							console.log("--TRANSITIONING: reset previous Frame");
+							previousFrameWindow.reset();
+						},function() {
+
+							$('.slide-iframe').removeClass('active');
+
+							$frame.addClass('active');
+
+							checkNewFrameReadyInterval = setInterval(function(){
+								if(frameWindow.isReady) {
+									frameWindow.enter();
+									clearInterval(checkNewFrameReadyInterval);
+								}
+							},100);
+
+							
+						});
+					} else {
+
+						console.log('--TRANSITIONING: previous Frame doesn\'t have exit');
+
+						$('.slide-iframe').removeClass('active');
+
+						$frame.addClass('active');
+
+						checkNewFrameReadyInterval = setInterval(function(){
+							if(frameWindow.isReady) {
+								frameWindow.enter();
+								clearInterval(checkNewFrameReadyInterval);
+							}
+						},100);
+
+						setTimeout(function() {
+							console.log("--TRANSITIONING: reset previous Frame");
+							previousFrameWindow.reset();
+						},250);
+
+					}
+
+				} else {
+
+					console.log('--TRANSITIONING: previous Frame hasn\'t finished yet');
+
+					$('.slide-iframe').removeClass('active');
+
+					$frame.addClass('active');
+
+					checkNewFrameReadyInterval = setInterval(function(){
+						if(frameWindow.isReady) {
+							frameWindow.enter();
+							clearInterval(checkNewFrameReadyInterval);
+						}
+					},100);
+
+					setTimeout(function() {
+						console.log("--TRANSITIONING: reset previous Frame");
+						previousFrameWindow.reset();
+					},250);
+
+				}
+			}
+
+			
+		},100);
+
+	}
+
+	window.imReady = function(target) {
+		if(target=='make-serious-money' && !initiated) {
+			console.log('enter make-serious-money');
+			window.frames['make-serious-money'].enter();
+			initiated = true;
+		}
 	}
 
 	$('.slide-selector').click(function(){
 		
 		var $self = $(this);
 
+		$('.slide-selector').removeClass('active');
+
+		$self.addClass('active');
 
 		enterSlide($self.data('target'));
-			
 
 	});
-
-
 
 });
