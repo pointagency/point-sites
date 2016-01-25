b16b30b6391a5f6eb502e9cc95d6fe4024539c8a
Andrew Pham <apham@point.agency>
2016-01-25 12:20:14 +1100

    Added welcome modal. Adjust header menu

--- hl-affiliates-account/compiled/acontent/js/highlow-affiliate.js
+++ hl-affiliates-account/compiled/acontent/js/highlow-affiliate.js
@@ -704,6 +704,148 @@ window.normalizeHeight = function(selector){
 	});
 }
 
+window.toggler = {
+	init: function() {
+
+		console.log("toggler");
+
+		$('.toggler').each(function(){
+			var self = $(this),
+			target = $(self.data('toggleTarget')),
+			focusTarget = $(self.data('toggleFocus')),
+			toggleClass = self.data('toggleClass'),
+			toggleGroup = self.data('toggleGroup'),
+			toggleBodyFixedHeight = self.data('toggleBodyFixedHeight'),
+			toggleMatchContainerHeight = self.data('toggleOnActiveMatchHeight'),
+			toggleSelfOffDisabled = self.data('toggleSelfOffDisabled'),
+			toggleOffClick = self.data('toggleOffClick');
+
+			if(!toggleClass || toggleClass == '') {
+				toggleClass = 'active';
+			}
+
+			if(self.data('toggleTarget')=='self') {
+				target = self;
+			}
+			var off = function() {
+				$('[data-toggle-target="'+self.data('toggleTarget')+'"]').removeClass(toggleClass);
+				target.removeClass(toggleClass);
+				if(toggleBodyFixedHeight) {
+					$('body').removeClass('fixed-height');
+				}
+				self.removeClass(toggleClass);
+			}
+
+			var on = function() {
+				if(toggleGroup && toggleGroup != '') {
+					$('[data-toggle-group="'+toggleGroup+'"]').removeClass(toggleClass);
+				}
+
+				focusTarget.focus();
+				focusTarget.select();
+				self.addClass(toggleClass);
+				target.addClass(toggleClass);
+				if(toggleBodyFixedHeight) {
+					$('body').addClass('fixed-height');
+				}
+				if(toggleMatchContainerHeight) {
+					$(toggleMatchContainerHeight).imagesLoaded(function(){
+						$(toggleMatchContainerHeight).css({
+							height: target.outerHeight()
+						});
+					});
+				}
+			}
+
+			var toggle = function() {
+				if(target.hasClass(toggleClass) && !toggleSelfOffDisabled ) {
+					off();
+				} else {
+					on();
+				}
+			}
+
+			if(self.data('hoverToggle')) {
+
+				var hoverHandler = function(){
+
+					console.log("ATTACH HOVER TOGGLE EVENT");
+
+					self.mouseover(function(){
+						if(!$('body').hasClass('touched')) {
+							on();
+						}
+					});
+					self.mouseout(function(){
+						if(!$('body').hasClass('touched') && !toggleSelfOffDisabled) {
+							off();
+						}
+					});
+
+					$(document).off('mousemove',hoverHandler);
+				}
+
+				$(document).on('mousemove', hoverHandler);
+
+				self.click(function(){
+					if($('body').hasClass('touched')) {
+						toggle();
+					}
+				});
+				
+				
+			} else {
+				self.click(function(){
+					toggle();
+				});
+			}
+
+			if(toggleOffClick) {
+				$(document).on('click', function(e) {
+					if(!$.contains(self.get(0),e.target) && e.target!==self.get(0) && !$(e.target).is(self.data('toggleTarget'))) {
+
+						if($(e.target).closest(self.data('toggleTarget')).length<=0) {
+							off();
+						}
+
+					}
+				})
+			}
+		});
+
+		function resize() {
+			$('.toggler.active').each(function(){
+				var self = $(this),
+				target = $(self.data('toggleTarget')),
+				toggleMatchContainerHeight = self.data('toggleOnActiveMatchHeight');
+
+				if(toggleMatchContainerHeight) {
+
+					$(toggleMatchContainerHeight).imagesLoaded(function(){
+						$(toggleMatchContainerHeight).css({
+							height: target.outerHeight()
+						});
+
+						setTimeout(function(){
+							$(toggleMatchContainerHeight).css({
+								height: target.outerHeight()
+							});
+						},750);
+					});
+				}
+
+			});
+		}
+	
+		$(window).resize(resize);
+
+		resize();
+
+		$('.disable-propagation').click(function(e){
+			e.stopPropagation();
+		});
+	}
+}
 
 
 $(document).ready(function(){
@@ -759,6 +901,7 @@ $(document).ready(function(){
 	UIinit();
 	resizeHandler.init();
 	scrollHandler.init();
+	window.toggler.init();
 
 	$('.flow-explain').addClass('concealed');
 
@@ -776,5 +919,10 @@ $(document).ready(function(){
 	$('.message-wrapper .close').click(function(){
 		$('.message-wrapper').removeClass('display').removeClass('fail').removeClass('success').removeClass('alert').removeClass('generic').removeClass('warning');
 	});	
+
+
+	if($('#first-time-welcome-message').length>0) {
+		$('#first-time-welcome-message').modal('show');
+	}
 
 });
